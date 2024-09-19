88f8cca8e24289d730cc88183e6a59e83b94c490
sodanhyun <166380533+sodanhyun@users.noreply.github.com>
2024-09-19 10:43:17 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,31 +1,15 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-// // import tailwindcss from '@headlessui/tailwindcss'
-//
-//
-// // https://vitejs.dev/config/
-//
-// export default defineConfig({
-//   plugins: [
-//     react(),
-//   ],
-//   define: {
-//     // By default, Vite doesn't include shims for NodeJS/
-//     // necessary for segment analytics lib to work
-//     global: {},
-//   },
-//   server: {
-//     proxy: {
-//       '/api': {
-//         //DEPLOY
-//         //target: 'https://3.35.239.151',
-//         //TEST
-//         target: 'http://localhost:8080',
-//         changeOrigin: true,
-//         rewrite: (path) => path.replace(/^\/api/, ''),
-//         secure: false,
-//         ws: true
-//       }
-//     }
-//   }
-// })
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+// import tailwindcss from '@headlessui/tailwindcss'
+
+
+// https://vitejs.dev/config/
+
+export default defineConfig({
+  plugins: [
+    react(),
+  ],
+    define: {
+        _global: ({})
+    }
+})
