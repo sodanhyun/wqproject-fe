53eb5c3f175612b20d7524a84a6a30e175adf99f
danhyeon <sodanhyun@gmail.com>
2024-10-15 17:45:31 +0900

    Update LectureReg.jsx

--- src/pages/lecture/LectureReg.jsx
+++ src/pages/lecture/LectureReg.jsx
@@ -155,20 +155,20 @@ const LectureReg = () => {
                         </span>
                         <div className="mt-2">
                           <div>
-                            {/* <label
+                            <label
                               htmlFor="starttimepicker"
                               className="inline-block text-sm font-medium leading-6 text-gray-900">
-                              시작 */}
+                              시작
                               <input
                                 value={reqData.sdate}
                                 onChange={onChangeHandler}
                                 name="sdate"
                                 type="datetime-local"
                                 id="starttimepicker"
-                                className="inline-block ml-3 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-500 sm:text-sm sm:leading-6 px-1.5 sm:"
+                                // className="inline-block ml-3 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-500 sm:text-sm sm:leading-6 px-1.5 sm:"
                                 min={new Date().toISOString().slice(0, 16)}
                               />
-                            {/* </label> */}
+                            </label>
                           </div>
                           <div className="mt-3">
                             <label
