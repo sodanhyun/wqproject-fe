28781f61c15fb6123100b9c1c15dcf13ba579dc0
sodanhyun <166380533+sodanhyun@users.noreply.github.com>
2024-12-09 14:15:20 +0900

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
-const formsPlugin = require('@tailwindcss/forms')
-const headlessuiPlugin = require('@headlessui/tailwindcss')
+import {formsPlugin} import '@tailwindcss/forms'
+import {headlessuiPlugin} import '@headlessui/tailwindcss'
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
@@ -44,5 +44,5 @@ module.exports = {
       
     },
   },
-  plugins: [formsPlugin, headlessuiPlugin,require('@tailwindcss/forms')],
+  plugins: [formsPlugin, headlessuiPlugin],
 }
